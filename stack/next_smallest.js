e90614775b266cbf2dad6ff3eb7399c8cc6def15
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-09-15 15:10:03 +0530

    Next Smallest

---
+++ stack/next_smallest.js
@@ -0,0 +1,46 @@
+function runProgram(input) {
+    let arr = input.trim().split(" ").map(Number);
+
+    let stack = [];
+    let output = [];
+
+    for (let i = arr.length-1; i >= 0; i--) {
+        if (stack.length === 0) {
+            output.push(-1);
+        } else if (stack.length > 0 && stack[stack.length-1] < arr[i]) {
+            output.unshift(stack[stack.length-1]);
+        } else if (stack.length > 0 && stack[stack.length-1] >= arr[i]) {
+            while (stack.length > 0 && stack[stack.length-1] >= arr[i]) {
+                stack.pop();
+            }
+            if (stack.length === 0) {
+                output.unshift(-1);
+            } else {
+                output.unshift(stack[stack.length-1]);
+            }
+        }
+        stack.push(arr[i]);
+    }
+    console.log(output.join(" "));
+}
+
+if (process.env.USERNAME === 'panka') {
+  runProgram("4 5 2 10 8");
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
