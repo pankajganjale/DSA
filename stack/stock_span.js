304b01f0d895623c8af857c260da7f69fb8ed77e
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-09-20 01:46:16 +0530

    Minor Change

--- stack/stock_span.js
+++ stack/stock_span.js
@@ -1,33 +1,47 @@
-function runProgram(input) {
-
+let find = (arr) => {
+  try {
     let output = [];
     let stack = [];
 
-    let arr = input.trim().split(" ").map(Number);
-
     for (let i = 0; i < arr.length; i++) {
-        if (stack.length === 0) {
-            output.push(1);
-        } else if (stack.length > 0 && stack[stack.length-1][0] >= arr[i]) {
-            output.push(i - stack[stack.length-1][1]);
-        } else if (stack.length > 0 && stack[stack.length-1][0] < arr[i]) {
-            while (stack[stack.length-1][0] < arr[i] && stack.length > 0) {
-                stack.pop()
-            }
-            if (stack.length === 0) {
-                output.push(1);
-            } else {
-                output.push(i - stack[stack.length-1][1]);
-            }
+      if (stack.length === 0) {
+        output.push(1);
+      } else if (stack.length > 0 && stack[stack.length - 1][0] > arr[i]) {
+        output.push(i - stack[stack.length - 1][1]);
+      } else if (stack.length > 0 && stack[stack.length - 1][0] <= arr[i]) {
+        while (stack.length > 0 && stack[stack.length - 1][0] <= arr[i]) {
+          stack.pop()
         }
-        stack.push([arr[i], i]);
+        if (stack.length === 0) {
+          output.push(i+1);
+        } else {
+          output.push(i - stack[stack.length - 1][1]);
+        }
+      }
+      stack.push([arr[i], i]);
     }
 
-    console.log(output);
+    return output.join(" ");
+  } catch (error) {
+    return error;
+  }
+}
+
+function runProgram(input) {
+  let inp = input.trim().split("\n");
+
+  for (let i = 2; i < inp.length; i += 2) {
+    let arr = inp[i].trim().split(" ").map(Number);
+    console.log(find(arr));
+  }
 }
 
 if (process.env.USERNAME === 'panka') {
-  runProgram("100 80 60 80 70 10 100");
+runProgram(`2
+7
+100 80 60 70 60 75 85
+5
+3 5 0 9 8`);
 } else {
   process.stdin.resume();
   process.stdin.setEncoding("ascii");
