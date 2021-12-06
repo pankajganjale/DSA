9f92050c13e747c261fda74675b41dcfa71abbe6
Pankaj Ganjale <pankajganjale2805@gmail.com>
2021-12-06 09:45:05 +0530

    added dp

--- dp/knapsack.js
+++ dp/knapsack.js
@@ -1,9 +1,10 @@
-let find = (values, items, w, n) => {
+let find = (values, items, w, n, empty) => {
     if (w === 0 || n === 0) return 0;
+    if (empty[n][w] !== -1) return empty[n][w];
     if (items[n-1] <= w) {
-        return Math.max(values[n-1] + find(values, items, w-items[n-1], n-1), find(values, items, w, n-1));
+        return empty[n][w] = Math.max(values[n-1] + find(values, items, w-items[n-1], n-1, empty), find(values, items, w, n-1, empty));
     } else {
-        return find(values, items, w, n-1);
+        return empty[n][w] = find(values, items, w, n-1, empty);
     }
 };
 
@@ -13,7 +14,8 @@ function runProgram(input) {
     let n = +input[1];
     let values = input[2].trim().split(" ").map(Number);
     let items = input[3].trim().split(" ").map(Number);
-    console.log(find(values, items, w, n));
+    let empty = Array(n+1).fill(Array(w+1).fill(-1));
+    console.log(find(values, items, w, n, empty));
 }
 
 if (process.env.USERNAME === 'panka') {
