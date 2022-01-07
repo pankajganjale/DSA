b159a21dc01137b08eea3b00ac1352914c2cb9ce
Pankaj Ganjale <pankajganjale2805@gmail.com>
2022-01-07 20:24:24 +0530

    Last Occurance

---
+++ binarySearch/lastOccurance.js
@@ -0,0 +1,23 @@
+const arr = [1, 3, 5, 10, 10, 10, 15];
+const n = arr.length;
+const k = 10;
+
+const firstOccurance = (arr, n, k) => {
+    let lo = 0;
+    let hi = n-1;
+    let index = -1;
+    while (lo < hi) {
+        const mid = Math.floor(lo + ((hi - lo)/2));
+        if (arr[mid] === k) {
+            index = mid;
+            lo = mid + 1;
+        } else if (arr[mid] < k) {
+            li = mid + 1;
+        } else {
+            hi = mid - 1;
+        }
+    }
+    return index;
+};
+
+console.log(firstOccurance(arr, n, k));
