159012dab76ecf974c75a4da36e8a17c7ba35c61
Pankaj Ganjale <pankajganjale2805@gmail.com>
2022-01-09 20:03:55 +0530

    Upper Bound

---
+++ binarySearch/ceil.js
@@ -0,0 +1,23 @@
+const arr = [1, 2, 3, 4, 6, 7, 9];
+const n = arr.length;
+const k = 5;
+
+const findFloor = (arr, n, k) => {
+    let lo = 0;
+    let hi = n-1;
+    let output = -1;
+    while (lo <= hi) {
+        const mid = Math.floor(lo + ((hi - lo) / 2));
+        if (arr[mid] === k) {
+            return arr[mid];
+        } else if (arr[mid] > k) {
+            output = arr[mid];
+            hi = mid - 1;
+        } else if (arr[mid] < k) {
+            lo = mid + 1;
+        }
+    }
+    return output;
+}
+
+console.log(findFloor(arr, n, k));
