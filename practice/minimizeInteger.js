846631a79da5f55b593e2584d46afd07de184fb6
Pankaj Ganjale <pankajganjale2805@gmail.com>
2022-01-17 11:58:41 +0530

    Done

--- practice/minimizeInteger.js
+++ practice/minimizeInteger.js
@@ -1,73 +1,68 @@
-const find = (arr, str) => {
-    // let n = +arr.join("");
-    
-    if (str === "negative") {
+const find = (arr) => {
+    if (arr[0] === "-") {
+        arr = arr.slice(1, arr.length);
         arr.sort((a, b) => b - a);
-        arr.unshift("-");
-        return +arr.join("");
-    }
-    
-    let zeros = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 0) {
-            zeros++;
+        return "-"+arr.join("");
+    } else {
+        let out = [];
+        let zeros = 0;
+        arr.sort((a, b) => b - a);
+        for (let i = arr.length-1; i >= 0; i--) {
+            if (arr[i] == 0) {
+                zeros++;
+                arr.pop();
+            } else {
+                break;
+            }
         }
-    }
-    if (zeros === arr.length) return +arr.join("");
-
-    arr.sort((a, b) => a - b);
-    let index = 0;
-    while (arr[index] === 0) {
-        index++;
-    }
-    let zeroIndex = index;
-    // let newIndex = 0
-    let compare = arr[index];
-    while (compare === arr[index]) {
-        index++;
-    }
-    let output = [...arr.slice(zeroIndex, index), ...arr.slice(0, zeroIndex), ...arr.slice(index)];
-    return +output.join("");
-}
-
-function runProgram(input) {
-    input = input.trim().split("\n");
-    for (let i = 1; i < input.length; i++) {
-        let num = input[i].trim().split("")
-        if (num[0] === "-") {
-            num.shift();
-            console.log(find(num.map(Number), "negative"));
-        } else {
-            console.log(find(num.map(Number), "positive"));
+        let k = arr[arr.length-1];
+        arr.pop();
+     //   console.log(arr)
+       out.push(k);
+        for (let i = 0; i < zeros; i++) {
+            out.push(0);
         }
-        
+        for (let i = arr.length-1; i >= 0; i--) {
+            out.push(arr[i]);
+        }
+        return out.join("");
     }
-}
-
-if (process.env.USERNAME === 'panka') {
-  runProgram(`2
-  53334121
-  -1002911
-  000000
-  1520005
-  `);
-} else {
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  let read = "";
-  process.stdin.on("data", function (input) {
-    read += input;
-  });
-  process.stdin.on("end", function () {
-    read = read.replace(/\n$/, "");
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-  });
-  process.on("SIGINT", function () {
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-    process.exit(0);
-  });
-}
-
-
+ };
+ 
+ function runProgram(input) {
+     input = input.trim().split("\n");
+     for (let i = 1; i < input.length; i++) {
+         let num = input[i].trim().split("");
+         console.log(find(num));
+         
+     }
+ }
+ 
+ if (process.env.USERNAME === 'panka') {
+   runProgram(`2
+   53334121
+   -1002911
+   000000
+   1520005
+   `);
+ } else {
+   process.stdin.resume();
+   process.stdin.setEncoding("ascii");
+   let read = "";
+   process.stdin.on("data", function (input) {
+     read += input;
+   });
+   process.stdin.on("end", function () {
+     read = read.replace(/\n$/, "");
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+   });
+   process.on("SIGINT", function () {
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+     process.exit(0);
+   });
+ }
+ 
+ 
+ 
