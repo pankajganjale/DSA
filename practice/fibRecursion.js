3d6743d0c27801ab6dce04d9afe6c198c28a85ee
Pankaj Ganjale <pankajganjale2805@gmail.com>
2022-01-20 09:42:43 +0530

    Done

---
+++ practice/fibRecursion.js
@@ -0,0 +1,32 @@
+const find = (n) => {
+    if (n < 1) return 0;
+    if (n === 1) return 1;
+    return find(n-1) + find(n-2);
+};
+
+function runProgram(input) {
+    console.log(find(+input));
+}
+
+if (process.env.USERNAME === 'panka') {
+  runProgram();
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
+
+
