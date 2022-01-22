5d86b8605e3f1980764a13a10d60876da5f81b0c
Pankaj Ganjale <pankajganjale2805@gmail.com>
2022-01-22 10:29:10 +0530

    Practice

---
+++ linked_list/practice.js
@@ -0,0 +1,29 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+    insertAtTail (data) {
+        const newNode = new Node(data);
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+}
+
+let one = new LinkedList()
+one.insertAtTail(5)
+one.insertAtTail(6)
+console.log(one)
